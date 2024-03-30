d5febae5e4cfcb5b1d6e6ec2f2e8f3dbc2d0f208
codestuffs <138683592+blenderstuffs0903@users.noreply.github.com>
2024-03-30 21:36:51 +0530

    Final keyword

--- Final/src/Main.java
+++ Final/src/Main.java
@@ -5,6 +5,5 @@ public class Main {
         Final finalClass = new Final();
         System.out.println(finalClass.X);
         finalClass.finalMethod();
-        System.out.println("YO");
     }
 }
