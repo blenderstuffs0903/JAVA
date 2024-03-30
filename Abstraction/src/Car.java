259afbd2b1381619931a11a6d68298830422fe20
codestuffs <138683592+blenderstuffs0903@users.noreply.github.com>
2024-03-30 21:41:01 +0530

    Abstraction

---
+++ Abstraction/src/Car.java
@@ -0,0 +1,5 @@
+public class Car extends Vehicle{
+    public void go() {
+        System.out.println("The driver is driving the car.");
+    }
+}
---
+++ Abstraction/src/Vehicle.java
@@ -0,0 +1,4 @@
+// abstract class is a class that cannot be instantiated but can be inherited. The subclass can be instanciated.
+abstract public class Vehicle {
+    abstract public void go(); // Abstract methods must be overriden by subclasses
+}
